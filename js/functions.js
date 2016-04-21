3dc7474f41ff6c1f7cadeaf7bba637e6540200c4
alicankustemur <alican.kustemur@gmail.com>
2016-04-22 00:44:47 +0300

    some updates

--- js/functions.js
+++ js/functions.js
@@ -217,7 +217,7 @@ function getAutoComplete(){
           if(event.which == 13 || event.keyCode == 13){
             $search = $(".search").val();
             location.href="tags.html?search="+$search;
-            alert(true);
+            alert("girilen değer aranıyor..");
           }
 
     });
